2b4dd4600331f4788853d9bfad2198905cb7957d
Artem Bakhanov <artembakhanov@gmail.com>
2019-11-17 17:33:34 +0300

    Add data generator

---
+++ DataGenerator/DataGenerator.py
@@ -0,0 +1,8 @@
+class DataGenerator:
+    def __init__(self):
+        """Empty constructor"""
+        pass
+
+    def generate(self, n):
+        """Generates some data with amount n."""
+        pass
